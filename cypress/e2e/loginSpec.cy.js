f4e485f39722d9b4fd749b0fd500d9891be7e45b
xXFranzFXx <franz.fernando@testpro.io>
2024-03-10 21:42:01 -0700

    Update loginSpec.cy.js

--- cypress/e2e/loginSpec.cy.js
+++ cypress/e2e/loginSpec.cy.js
@@ -1,26 +1,30 @@
 describe('Log in tests', () => {
-  it('successfully log the user in', () => {
-    cy.visit('/');
+ context('Login using the UI', () => {
+    it('successfully log the user in', () => {
+      cy.visit('/');
 
-    cy.get("[type='email']")
-      .type(Cypress.env('user'), { force: true })
-      .type("{enter}");
+      cy.get("[type='email']")
+        .type(Cypress.env('user'), { force: true })
+        .type("{enter}");
 
-    cy.get("[type='password']")
-      .type(Cypress.env('password'), { force: true })
-      .type("{enter}");
+      cy.get("[type='password']")
+        .type(Cypress.env('password'), { force: true })
+        .type("{enter}");
 
-    cy.get("button[type='submit']").click();
-  });
+      cy.get("button[type='submit']").click();
+    });
+});
 
-  it('log in through api and save token from response body', () => {
-    cy.request('POST', 'https://qa.koel.app/api/me', {
-    email: Cypress.env('user'),
-    password: Cypress.env('password')
-  }).then(
-    (response) => {
-      expect(response.body).to.have.property('token');
-      cy.writeFile('cypress/fixtures/token.json', response.body);
-     });
+  context('Login using the api', () => {
+    it('save token from response body', () => {
+      cy.request('POST', 'https://qa.koel.app/api/me', {
+      email: Cypress.env('user'),
+      password: Cypress.env('password')
+    }).then(
+      (response) => {
+        expect(response.body).to.have.property('token');
+        cy.writeFile('cypress/fixtures/token.json', response.body);
+      });
+    });
   });
 })
