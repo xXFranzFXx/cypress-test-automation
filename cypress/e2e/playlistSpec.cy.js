b2b7602bd3ded964a20899c9e0228ad99310efba
xXFranzFXx <franz.fernando@testpro.io>
2024-03-11 18:38:29 -0700

    Update playlistSpec.cy.js

--- cypress/e2e/playlistSpec.cy.js
+++ cypress/e2e/playlistSpec.cy.js
@@ -19,59 +19,62 @@ require('cypress-xpath');
 describe('User Playlist Tests', () => {
     beforeEach(() => {
         cy.viewport(1280, 720);
-        cy.loginWithValidCredentials(Cypress.env('user'), Cypress.env('password'));
+        cy.loginWithValidCredentials(Cypress.env('user'), Cypress.env('password')); 
     });
 
-    it('create a new playlist', () => {
-        const playlist = Cypress.env('playlist');
-        createNewPlaylist();
-        clickNewPlaylistContextMenu();
-        enterPlaylistName(playlist);
-        assertSuccessNotificationDisplayed(playlist);
+   context('Regular playlists', () => {
+        it('create a new playlist', () => {
+            const playlist = Cypress.env('playlist');
+            createNewPlaylist();
+            clickNewPlaylistContextMenu();
+            enterPlaylistName(playlist);
+            assertSuccessNotificationDisplayed(playlist);
+        });
+
+        it('add a song to a playlist', () => {
+            const playlistWithSong = Cypress.env('playlistWithSong');
+            clickAllSongs();
+            cy.get('table > tr').find('td')
+                .then(
+                    (el) => {
+                        cy.get(el)
+                            .first()
+                            .click();
+
+                        cy.get('span[class="btn-group"] > button[data-test="add-to-btn"]')
+                            .should('be.visible')
+                            .click();
+
+                        cy.get('input[data-test="new-playlist-name"]')
+                            .first()
+                            .type(`${playlistWithSong}`);
+
+                        cy.get('button[type="submit"]')
+                            .first()
+                            .click();
+
+                        assertSuccessNotificationDisplayed('Created');
+                    });
+        });
+
+        it('delete all regular playlists', () => {
+            contextClickDeletePlaylists();
+            checkUserPlaylists();
+        });
     });
 
-    it('create a new smart playlist', () => {
-        const smartPlaylist = Cypress.env('smartPlaylist');
-        const smartPlaylistRule = Cypress.env('smartPlaylistRule');
-        createNewPlaylist();
-        clickNewSmartPlaylistContextMenu();
-        enterNewSmartPlaylistInfo(`${smartPlaylist}`, `${smartPlaylistRule}`);
-        assertSuccessNotificationDisplayed(`${smartPlaylist}`);
-    });
+    context('Smart playlists', () => {
+            it('create a new smart playlist', () => {
+                const smartPlaylist = Cypress.env('smartPlaylist');
+                const smartPlaylistRule = Cypress.env('smartPlaylistRule');
+                clickNewSmartPlaylistContextMenu();
+                enterNewSmartPlaylistInfo(`${smartPlaylist}`, `${smartPlaylistRule}`);
+                assertSuccessNotificationDisplayed(`${smartPlaylist}`);
+            });
 
-    it('add a song to a playlist', () => {
-        const playlistWithSong = Cypress.env('playlistWithSong');
-        clickAllSongs();
-        cy.get('table > tr').find('td')
-            .then(
-                (el) => {
-                    cy.get(el)
-                        .first()
-                        .click();
-
-                    cy.get('span[class="btn-group"] > button[data-test="add-to-btn"]')
-                        .should('be.visible')
-                        .click();
-
-                    cy.get('input[data-test="new-playlist-name"]')
-                        .first()
-                        .type(`${playlistWithSong}`);
-
-                    cy.get('button[type="submit"]')
-                        .first()
-                        .click();
-
-                    assertSuccessNotificationDisplayed('Created');
-                });
-    });
-
-    it('delete all regular playlists', () => {
-       contextClickDeletePlaylists();
-       checkUserPlaylists();
-    });
-
-    it('delete all smart playlists', () => {
-        contextClickDeleteSmartPlaylists();
-        checkUserPlaylists();
-    });
+            it('delete all smart playlists', () => {
+                contextClickDeleteSmartPlaylists();
+                checkUserPlaylists();
+            });
+        });
 });
