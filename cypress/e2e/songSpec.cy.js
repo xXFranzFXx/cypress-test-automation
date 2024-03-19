d4273cdc801c04d0e7fd944879c7252e62c50495
xXFranzFXx <franz.fernando@testpro.io>
2024-03-19 09:04:03 -0700

    Update songSpec.cy.js

--- cypress/e2e/songSpec.cy.js
+++ cypress/e2e/songSpec.cy.js
@@ -4,39 +4,41 @@ describe('Songs tests', () => {
   beforeEach(() => {
     cy.loginWithValidCredentials(Cypress.env('user'), Cypress.env('password'))
     });
+  
+  context('Downloading songs', () => {
+      it('download a song and verify the file downloaded', () => {
+        clickAllSongs();
 
-  it('download a song and verify the file downloaded', () => {
-   clickAllSongs();
-
-   cy.get('table > tr')
-    .find('td')
-    .then(
-      (el) => {
-
-       cy.get(el)
-        .first()
-        .rightclick();
-
-       cy.get('[data-testid="song-context-menu"]')
-        .contains('Download')
-        .click();
-
-    }).verifyFileDownload('.mp3');
-  });
-
-  it('play a song that has been downloaded', () => {
-    cy.task('getFirstFile', 'cypress/downloads')
-      .then(
-        (file) => {
-
-          cy.readFile('cypress/downloads/' + file, 'base64')
+        cy.get('table > tr')
+          .find('td')
           .then(
-            (mp3) => {
+            (el) => {
 
-              const uri = 'data:audio/mp3;base64,' + mp3
-              const audio = new Audio(uri)
-              audio.play()
+              cy.get(el)
+                .first()
+                .rightclick();
+
+              cy.get('[data-testid="song-context-menu"]')
+                .contains('Download')
+                .click();
+
+          }).verifyFileDownload('.mp3');
+      });
+
+      it('play a song that has been downloaded', () => {
+        cy.task('getFirstFile', 'cypress/downloads')
+          .then(
+            (file) => {
+
+              cy.readFile('cypress/downloads/' + file, 'base64')
+              .then(
+                (mp3) => {
+
+                  const uri = 'data:audio/mp3;base64,' + mp3
+                  const audio = new Audio(uri)
+                  audio.play()
+                });
             });
-         });
-     });
+        });
+    });
 });
