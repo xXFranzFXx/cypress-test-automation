9a73eac1ef3e3ec4123d5b5caca1aa735947194d
xXFranzFXx <franz.fernando@testpro.io>
2024-04-11 22:27:33 -0700

    Update searchSpec.cy.js

--- cypress/e2e/searchSpec.cy.js
+++ cypress/e2e/searchSpec.cy.js
@@ -8,42 +8,43 @@ describe('Search feature functionality tests', () => {
     // assertRecentlyPlayedVisibility();
     // assertPlaylistsSMVisibility();
   });
-  
-  it('search for a song using the search bar and verify information is displayed in the search results', () => {
-    verifySearchInHeader(word);
-    compareUrl('search');
-  });
-
-  it('clear the search field by using the cancel button', () => {
-    verifySearchInHeader(word);
-    compareUrl('search');
-    cy.get('#searchForm > input').then(
-      ($element) => {
-        const width = $element.width();
-        const height = $element.height();
-        const center = width/2;
-        const xOffset = center - 10;
-        cy.get($element)
-          .click(xOffset, -1, {force: true})
-          .should('be.empty');
-    });
-});
-
-  it('invoke search feature by pressing "F" key', () => {
-      invokeSearchByKeybd();
-      verifySearchInHeader(word);
+  context('Verify search bar functionality', () => {
+    it('search for a song using the search bar and verify information is displayed in the search results', () => {
+     verifySearchInHeader(word);
       compareUrl('search');
-  });
-  
-  it('clears the previous search in the search bar using keyboard combination', () => {
-    invokeSearchByKeybd()
-      cy.get('#searchForm > input')
-        .should('have.focus')
-        .then((el) => {
-          cy.get(el)
-            .type(word)
-            .type('{ctrl+shift+a}{backspace}')
-            .should('be.empty');
-        });
     });
+
+    it('clear the search field by using the cancel button', () => {
+     verifySearchInHeader(word);
+      compareUrl('search');
+      cy.get('#searchForm > input').then(
+        ($element) => {
+          const width = $element.width();
+          const height = $element.height();
+          const center = width/2;
+          const xOffset = center - 10;
+          cy.get($element)
+            .click(xOffset, -1, {force: true})
+            .should('be.empty');
+      });
+  });
+
+    it('invoke search feature by pressing "F" key', () => {
+        invokeSearchByKeybd();
+        verifySearchInHeader(word);
+        compareUrl('search');
+    });
+    
+    it('clears the previous search in the search bar using keyboard combination', () => {
+      invokeSearchByKeybd()
+        cy.get('#searchForm > input')
+          .should('have.focus')
+          .then((el) => {
+            cy.get(el)
+              .type(word)
+              .type('{ctrl+shift+a}{backspace}')
+              .should('be.empty');
+          });
+      });
+  });
 });
