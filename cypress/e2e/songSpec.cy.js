e311398b41ff77138981dea82fc2581e97760c59
xXFranzFXx <franz.fernando@testpro.io>
2024-04-25 13:19:49 -0700

    updates songSpec
    adds fs dependency

--- cypress/e2e/songSpec.cy.js
+++ cypress/e2e/songSpec.cy.js
@@ -1,10 +1,10 @@
 import { assertRecentlyPlayedVisibility, assertPlaylistsSMVisibility, verifySearchInHeader, compareUrl, invokeSearchByKeybd, clickAllSongs } from "../support/e2e";
-
+import fs from 'fs';
 describe('Songs tests', () => {
   beforeEach(() => {
     cy.loginWithValidCredentials(Cypress.env('user'), Cypress.env('password'))
     });
-  
+ 
   context('Downloading songs', () => {
       it('download a song and verify the file downloaded', () => {
         clickAllSongs();
@@ -29,16 +29,21 @@ describe('Songs tests', () => {
         cy.task('getFirstFile', 'cypress/downloads')
           .then(
             (file) => {
-
               cy.readFile('cypress/downloads/' + file, 'base64')
               .then(
                 (mp3) => {
-
                   const uri = 'data:audio/mp3;base64,' + mp3
                   const audio = new Audio(uri)
                   audio.play()
                 });
             });
+    context('Delete downloaded songs', () => {
+      it('delete all songs in download directory', () => {
+        fs.readdirSync('../downloads').forEach(file => {
+          fs.unlinkSync(`../downloads/${file}`);
+        })
+      })
+    })
         });
     });
 });
